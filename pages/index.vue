2fb36fbcef13a80ffe5bcb2223ff0ec5918c4f59
titancube <kbin0717@gmail.com>
2021-03-07 05:19:14 +0900

    style: minor style change

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container items-center text-center">
+  <div class="container items-center text-center w-2/3 md:w-full">
     <div>
-      <h1 class="title">Shoredark Control Panel</h1>
+      <h1 class="w-2/3">Shoredark Control Panel</h1>
     </div>
   </div>
 </template>
@@ -16,15 +16,5 @@ export default class MilAdd extends Vue {}
 <style lang="postcss">
 .container {
   @apply m-auto my-0 min-h-screen flex justify-center pt-20 w-full pb-2;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 </style>
