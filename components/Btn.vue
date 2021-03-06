7bbc4646cf3fa88af3599eafe467831718ca4ee2
titancube <kbin0717@gmail.com>
2021-03-07 05:18:25 +0900

    style: change hover color

--- components/Btn.vue
+++ components/Btn.vue
@@ -1,12 +1,12 @@
 <template>
   <button
-    class="p-2 px-4 rounded-md m-1 focus:outline-none focus:ring-4"
+    class="p-2 px-4 rounded-md m-1 focus:outline-none outline-none focus:ring-4 duration-150"
     :class="[
       category == 0
-        ? 'bg-gray-700 hover:bg-gray-400'
+        ? 'bg-gray-700 hover:bg-gray-500'
         : category == 1
-        ? 'bg-blue-700 hover:bg-blue-400'
-        : 'bg-red-700 hover:bg-red-400',
+        ? 'bg-blue-700 hover:bg-blue-500'
+        : 'bg-red-700 hover:bg-red-500',
     ]"
   >
     {{ name }}
