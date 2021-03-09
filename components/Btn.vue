a435ed5947c67f9ddc8afeb57b12a72977ea544e
Titancube <kbin0717@gmail.com>
2021-03-09 17:00:44 +0900

    fix: add function to properly change button color

--- components/Btn.vue
+++ components/Btn.vue
@@ -1,13 +1,7 @@
 <template>
   <button
     class="p-2 px-4 rounded-md m-1 focus:outline-none outline-none focus:ring-4 duration-150"
-    :class="
-      category === 0
-        ? ['bg-gray-700', 'hover:bg-gray-500']
-        : category === 1
-        ? ['bg-blue-700', 'hover:bg-blue-500']
-        : ['bg-red-700', 'hover:bg-red-500']
-    "
+    :class="setButtonColor"
   >
     {{ name }}
   </button>
@@ -20,6 +14,18 @@ import { Component, Vue, Prop } from 'nuxt-property-decorator'
 export default class Button extends Vue {
   @Prop(Number) category: number
   @Prop(String) name: string | number
+
+  get setButtonColor() {
+    switch (this.category) {
+      default:
+      case 0:
+        return ['bg-gray-700', 'hover:bg-gray-500']
+      case 1:
+        return ['bg-blue-700', 'hover:bg-blue-500']
+      case 2:
+        return ['bg-red-700', 'hover:bg-red-500']
+    }
+  }
 }
 </script>
 
