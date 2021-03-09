7a79831d6cf18af753d603ad48de9aadd7ace277
Titancube <kbin0717@gmail.com>
2021-03-09 16:50:32 +0900

    fix: strict check on category

--- components/Btn.vue
+++ components/Btn.vue
@@ -2,11 +2,11 @@
   <button
     class="p-2 px-4 rounded-md m-1 focus:outline-none outline-none focus:ring-4 duration-150"
     :class="
-      category == 0
+      category === 0
         ? ['bg-gray-700', 'hover:bg-gray-500']
-        : category == 1
+        : category === 1
         ? ['bg-blue-700', 'hover:bg-blue-500']
-        : ['bg-red-700', 'hover:bg-red-500'],
+        : ['bg-red-700', 'hover:bg-red-500']
     "
   >
     {{ name }}
