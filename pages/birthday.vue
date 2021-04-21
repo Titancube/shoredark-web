4909e40a23ac1d58b582860f52857b991b0bc4ef
Titancube <kbin0717@gmail.com>
2021-04-21 11:41:15 +0900

    fix deps

--- pages/birthday.vue
+++ pages/birthday.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="container flex-wrap">
     <section class="w-full p-12">
-      <h1>전역카운터</h1>
+      <h1>생일</h1>
       <fieldset
         class="w-full flex flex-wrap items-center p-4 mt-12 mb-4 ring-1 duration-150"
-        :class="addSoldier.id ? 'ring-green-300' : 'ring-gray-500 '"
+        :class="addBirthday.id ? 'ring-green-300' : 'ring-gray-500 '"
       >
-        <transition name="push" mode="out-in">
+        <transition name="push" mode="">
           <legend
-            v-if="addSoldier.id"
+            v-if="addBirthday.id"
             key="1"
-            class="bg-gray-900 px-4 text-xl font-bold text-green-300 md:block hidden"
+            class="bg-gray-900 px-4 text-xl font-bold text-green-300 absolute transform -translate-y-10 md:block hidden"
           >
             수정 모드
           </legend>
           <legend
             v-else
             key="2"
-            class="bg-gray-900 px-4 text-xl font-bold text-gray-500 md:block hidden"
+            class="bg-gray-900 px-4 text-xl font-bold text-gray-500 absolute transform -translate-y-10 md:block hidden"
           >
             신규 등록
           </legend>
         </transition>
         <legend
           class="bg-gray-900 px-4 text-xl font-bold md:hidden"
-          :class="addSoldier.id ? 'text-green-300' : 'text-gray-500'"
+          :class="addBirthday.id ? 'text-green-300' : 'text-gray-500'"
         >
-          {{ addSoldier.id ? '수정 모드' : '신규 등록' }}
+          {{ addBirthday.id ? '수정 모드' : '신규 등록' }}
         </legend>
         <div
           class="flex justify-between items-center w-full md:px-2 md:py-2 md:w-1/4 mb-4 md:mb-0"
@@ -37,26 +37,8 @@
             name="name"
             id="name"
             class="input-mil w-2/3"
-            v-model="addSoldier.name"
+            v-model="addBirthday.name"
           />
-        </div>
-        <div
-          class="flex justify-between items-center w-full md:px-2 md:py-2 md:w-1/4 mb-4 md:mb-0"
-        >
-          <label for="variant" class="mr-2 font-bold text-lg">분류</label>
-          <select
-            name="variant"
-            id="variant"
-            class="input-mil w-2/3"
-            v-model="addSoldier.variant"
-          >
-            <option value="" disabled selected hidden>선택</option>
-            <option value="육군" class="input-mil">육군</option>
-            <option value="해군" class="input-mil">해군</option>
-            <option value="공군" class="input-mil">공군</option>
-            <option value="공익" class="input-mil">공익</option>
-            <option value="상근" class="input-mil">상근</option>
-          </select>
         </div>
         <div
           class="flex justify-between items-center w-full md:px-2 md:py-2 md:w-1/4 mb-4 md:mb-0"
@@ -67,20 +49,20 @@
             name="diacharge"
             id="diacharge"
             class="input-mil w-2/3"
-            v-model="addSoldier.dischargeDate"
+            v-model="addBirthday.birthday"
           />
         </div>
         <div class="flex items-center w-full md:px-2 md:py-2 md:w-1/4">
           <button
             class="outline-none focus:outline-none w-full md:w-auto py-1 px-4 text-sm bg-blue-600 font-bold rounded-sm"
-            @click="uploadSoldier"
+            @click="add"
           >
-            {{ addSoldier.id ? '수정' : '추가' }}
+            {{ addBirthday.id ? '수정' : '추가' }}
           </button>
           <button
-            v-if="addSoldier.id"
+            v-if="addBirthday.id"
             class="outline-none focus:outline-none w-full md:w-auto py-1 px-4 text-sm ml-1 bg-gray-800 font-bold rounded-sm"
-            @click="cancelModify"
+            @click="modCancel"
           >
             취소
           </button>
@@ -88,7 +70,7 @@
       </fieldset>
       <fieldset class="w-full p-4 mt-12 mb-4 ring-1 ring-gray-500">
         <legend class="bg-gray-900 px-4 text-xl font-bold text-gray-500">
-          복무중인 인원
+          모든 생일
         </legend>
         <table class="w-full">
           <thead>
@@ -103,27 +85,25 @@
           </thead>
           <tbody>
             <tr
-              v-for="s in activeSoldiers"
+              v-for="s in allBirthday"
               :key="s.id"
-              :class="{ 'bg-green-800': addSoldier.id === s.id }"
+              :class="{ 'bg-green-800': addBirthday.id === s.id }"
             >
               <td class="w-2/3 md:w-1/4">{{ s.name }}</td>
               <td class="md:w-1/4 hidden md:table-cell">{{ s.variant }}</td>
               <td class="md:w-1/4 hidden md:table-cell">
-                {{ s.dischargeDate }}
+                {{ s.birthday }}
               </td>
               <td class="w-1/3 md:w-1/4">
                 <button
                   class="outline-none focus:outline-none py-1 px-4 text-sm bg-blue-600 rounded-sm my-1 md:my-0"
-                  @click="
-                    modifySoldier(s.id, s.name, s.dischargeDate, s.variant)
-                  "
+                  @click="mod(s.id, s.name, s.birthday, s.variant)"
                 >
                   수정
                 </button>
                 <button
                   class="outline-none focus:outline-none py-1 px-4 text-sm bg-red-600 rounded-sm my-1 md:my-0"
-                  @click="deleteSoldier(s.id, s.name)"
+                  @click="del(s.id, s.name)"
                 >
                   삭제
                 </button>
@@ -143,42 +123,32 @@ import { format } from 'date-fns'
 interface Soldier {
   id: string
   name: string
-  dischargeDate: string
-  variant: string
+  birthday: string
 }
 
 @Component
-export default class military extends Vue {
-  activeSoldiers: Soldier[] = []
-  addSoldier: Soldier = {
+export default class birthday extends Vue {
+  allBirthday: Soldier[] = []
+  addBirthday: Soldier = {
     id: '',
     name: '',
-    dischargeDate: '',
-    variant: '',
-  }
-
-  layout() {
-    return 'main'
+    birthday: '',
   }
 
   mounted() {
-    this.getSoldiers()
+    this.get()
   }
 
   // Get soldiers from firestore
-  async getSoldiers() {
-    this.activeSoldiers = []
-    const r = await this.$fire.firestore.collection('Retire').get()
+  async get() {
+    this.allBirthday = []
+    const r = await this.$fire.firestore.collection('Birthday').get()
     try {
       r.forEach((doc) => {
-        this.activeSoldiers.push({
+        this.allBirthday.push({
           id: doc.id,
           name: doc.data().name,
-          dischargeDate: format(
-            doc.data().dischargeDate.toDate(),
-            'yyyy-MM-dd'
-          ),
-          variant: doc.data().variant,
+          birthday: format(doc.data().birthday.toDate(), 'yyyy-MM-dd'),
         })
       })
     } catch (e) {
@@ -186,83 +156,69 @@ export default class military extends Vue {
     }
   }
 
-  // Return parsed data of dischargeDate as Date
+  // Return parsed data of birthday as Date
   get toDate(): Date {
-    return new Date(this.addSoldier.dischargeDate)
+    return new Date(this.addBirthday.birthday)
   }
 
   // Modify soldier
-  modifySoldier(
-    id: string,
-    name: string,
-    dischargeDate: string,
-    variant: string
-  ) {
-    this.addSoldier = {
+  mod(id: string, name: string, birthday: string) {
+    this.addBirthday = {
       id,
       name,
-      dischargeDate,
-      variant,
+      birthday,
     }
   }
 
   // Cancel modify
-  cancelModify(): void {
-    this.addSoldier = {
+  modCancel(): void {
+    this.addBirthday = {
       id: '',
       name: '',
-      dischargeDate: '',
-      variant: '',
+      birthday: '',
     }
   }
 
   // Add a soldier
-  async uploadSoldier() {
-    const conditions = [
-      this.addSoldier.name,
-      this.addSoldier.dischargeDate,
-      this.addSoldier.variant,
-    ]
+  async add() {
+    const conditions = [this.addBirthday.name, this.addBirthday.birthday]
     if (conditions.includes('')) {
       alert('빠진 부분 없이 작성해주세요')
       return false
     } else {
       try {
-        if (!this.addSoldier.id) {
-          await this.$fire.firestore.collection('Retire').add({
-            name: this.addSoldier.name,
-            dischargeDate: this.toDate,
-            variant: this.addSoldier.variant,
+        if (!this.addBirthday.id) {
+          await this.$fire.firestore.collection('Birthday').add({
+            name: this.addBirthday.name,
+            birthday: this.toDate,
           })
         } else {
           await this.$fire.firestore
-            .collection('Retire')
-            .doc(this.addSoldier.id)
+            .collection('Birthday')
+            .doc(this.addBirthday.id)
             .update({
-              name: this.addSoldier.name,
-              dischargeDate: this.toDate,
-              variant: this.addSoldier.variant,
+              name: this.addBirthday.name,
+              birthday: this.toDate,
             })
         }
 
         alert(`
     등록되었습니다
 
-    이름: ${this.addSoldier.name}
-    전역일: ${this.addSoldier.dischargeDate}
-    종류: ${this.addSoldier.variant}
+    이름: ${this.addBirthday.name}
+    생일: ${this.addBirthday.birthday}
     `)
       } catch (e) {
         alert(`너는 수장이 아니로구나`)
       } finally {
-        this.cancelModify()
-        this.getSoldiers()
+        this.modCancel()
+        this.get()
       }
     }
   }
 
   // Delete soldier
-  async deleteSoldier(id: string, name: string) {
+  async del(id: string, name: string) {
     if (
       confirm(`
     삭제하시겠습니까?
@@ -271,12 +227,12 @@ export default class military extends Vue {
     `)
     ) {
       try {
-        await this.$fire.firestore.collection('Retire').doc(id).delete()
+        await this.$fire.firestore.collection('Birthday').doc(id).delete()
         alert('삭제되었습니다')
       } catch (e) {
         alert(`너는 수장이 아니로구나`)
       } finally {
-        this.getSoldiers()
+        this.get()
       }
     }
   }
@@ -342,15 +298,17 @@ p {
 
 .push-enter-active,
 .push-leave-active {
-  transition: all 0.05s;
+  position: absolute;
+  transition: all 0.15s;
 }
 
 .push-enter {
-  transform: translate(0, -10px);
+  @apply -translate-y-12;
   opacity: 0;
 }
+
 .push-leave-to {
-  transform: translate(0, 10px);
+  @apply -translate-y-8;
   opacity: 0;
 }
 </style>
